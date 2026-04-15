1145e721290ab57661bdd837788e6eff4c8e162b
akarshv21 <akarshv.work@gmail.com>
2026-04-15 14:03:56 +0530

    [akarsh].final fix

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -7,7 +7,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
-  
+
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
